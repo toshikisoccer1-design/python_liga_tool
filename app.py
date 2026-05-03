d8b6353437d818a428cd83d145103309b2ab922d
中村利記 <nakamuratoshiki@nakamurarikinoMacBook-Pro.local>
2026-05-04 01:30:33 +0900

    fix database init

--- app.py
+++ app.py
@@ -122,7 +122,8 @@ def delete_customer_api(id):
     return jsonify({"status": "ok"})
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
